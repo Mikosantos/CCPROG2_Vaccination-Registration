efb13bfec5a492e4d51efbae98e14b0af5b77d36
Miko Santos <mikosantos334@gmail.com>
2023-04-24 17:57:36 +0800

    Update SantosMikoL.c

--- SantosMikoL.c
+++ SantosMikoL.c
@@ -1,12 +1,3 @@
-/*********************************************************************************************************	
-This is	to certify that	this project is my own work, based	on	my	personal efforts in	studying and applying the concepts	
-learned. I have	constructed	the	functions and their	respective algorithms and corresponding	code by	myself.	 The	
-program	was	run, tested, and debugged by my	own	efforts. I further certify	that I have	not	copied	in	part or	whole or	
-otherwise plagiarized	the	work of	other students and/or persons.
-
-	  														SANTOS, MIKO LUMINTAC,	DLSU	ID#	12256170	
-*********************************************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
